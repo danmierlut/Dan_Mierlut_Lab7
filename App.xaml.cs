f400e4b3f32d345562bf40b1ae7908e90fc74d55
GAVRIL-DANIEL MIERLUȚ <gavril.mierlut@stud.ubbcluj.ro>
2023-12-20 11:27:37 +0200

    Lab 8 as branch of Lab7

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace Dan_Mierlut_Lab7
+﻿using System;
+using Dan_Mierlut_Lab7.Data;
+using System.IO;
+
+namespace Dan_Mierlut_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database 
+        { 
+            get 
+            { 
+                if (database == null) 
+                { 
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
